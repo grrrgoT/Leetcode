ea5c53e5aca0c44a3927cf2a6d6605eeef42d86d
Go Terauchi <goterauchi5@gmail.com>
2026-06-11 22:05:22 +0900

    invert binary tree

---
+++ EASY/INVERT_BINARY_TREE.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def invertTree(self, root):
+        if not root:
+            return
+        
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
