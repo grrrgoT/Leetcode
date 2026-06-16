347f6177a3079c53567a57ce6529cd7ca51594fe
Go Terauchi <goterauchi5@gmail.com>
2026-06-16 23:26:03 +0900

    last stone weight

---
+++ EASY/LAST_STONE_WEIGHT.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def lastSToneWeight(self, stones):
+        h = [-x for x in stones]
+        heapq.heapify(h)
+        while len(h) > 1 and h[0] != 0:
+            heapq.heappush(h, heapq.heappop(h) - heapq.heappop(h))
+        return -h[0]
+    
