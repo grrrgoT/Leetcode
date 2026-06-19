660965e16567f7753b449a589567b5aecf7d867b
Go Terauchi <goterauchi5@gmail.com>
2026-06-19 23:15:15 +0900

    happy number

---
+++ EASY/HAPPY_NUMBER.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isHappy(self, n):
+        if ( n == 1 or n ==7):
+            return True
+        elif (n < 10):
+            return False
+        else:
+            sum = 0
+            while ( n > 10 ):
+                temp = n % 10
+                sum += temp * temp
+                n = n // 10
+
+            return self.isHappy(sum)
+        
