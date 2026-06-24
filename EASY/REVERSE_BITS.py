f2e58061e78e75d0e86a5610023cab7d61e0d237
Go Terauchi <goterauchi5@gmail.com>
2026-06-24 21:03:56 +0900

    reverse bits

---
+++ EASY/REVERSE_BITS.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def reverseBits(self, n):
+        res = 0
+        for i in range(32):
+            res = (res << 1) | (n & 1)
+            n >>= 1
+
+        return res
+    
+
